737462cf3384aee64b2e666d08128164d1400266
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-11-01 10:44:10 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vue Simple Calculator
+
+Nama : Samuel Yuma Krismata
+NRP : 5027221029
+Kelas : B
+
+## Link Website (Github Page)
+
+## Link Website (Vercel)
+
+## Link Website (Netlify)
---
+++ src/views/FirstCalculator copy.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <h1>First Calculator 🧮</h1>
+
+    <input type="number" v-model.number="first" placeholder="Input here" />
+    <input type="number" v-model.number="second" placeholder="Input here" />
+
+    <br />
+    <br />
+
+    <input type="button" value="+" @click="addCalc" />
+    <input type="button" value="-" @click="subCalc" />
+    <input type="button" value="*" @click="multiCalc" />
+    <input type="button" value="/" @click="divCalc" />
+
+    <br />
+    <br />
+
+    <input v-model="result" readonly />
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      first: '',
+      second: '',
+      result: ''
+    }
+  },
+  methods: {
+    addCalc() {
+      this.result = this.first + this.second
+    },
+    subCalc() {
+      this.result = this.first - this.second
+    },
+    multiCalc() {
+      this.result = this.first * this.second
+    },
+    divCalc() {
+      this.result = this.first / this.second
+    }
+  }
+}
+</script>
