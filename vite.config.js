6549bf9aa98d34b73f12c6f78536b822e886ada0
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-11-01 11:26:12 +0700

    fix: base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/yuma-vue-calculator/',
+  base: '/vue-simple-calculator/',
   plugins: [vue()],
   resolve: {
     alias: {
